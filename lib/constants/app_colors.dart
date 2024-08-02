2e94487e78a47578428dbba3a7ce1115317063ab
vishelpodishat <alishsaideshov@gmail.com>
2024-08-02 17:08:17 +0500

    added app fonts and colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xffF2F2F7);
+  static const Color primary = Color(0xff052DC1);
+  static final Color primaryDisabled = Color(0xff052DC1).withOpacity(0.5);
+  static const Color blackForText = Color(0xff32384A);
+  static const Color grayForText = Color(0xff8E8E93);
+  static const Color blockOnTheWhite = Color(0xffE5E5EA73);
+  static const Color whiteForText = Color(0xffFFFFFF);
+  static final Color choiceColor = Color(0xff052DC126);
+  static final Color cardOnTheBlue = Color(0xffE5E5EA4D);
+  static final Color cardOnTheBlue2 = Color(0xffE5E5EAB2);
+}
---
+++ lib/constants/app_text_style.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+const String kInterFontFamily = 'Inter';
+
+class AppTextStyle {
+  static const TextStyle heading1 = TextStyle(
+    fontFamily: kInterFontFamily,
+    fontSize: 24,
+    fontWeight: FontWeight.w800,
+  );
+
+  static const TextStyle heading2 = TextStyle(
+    fontFamily: kInterFontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle heading3 = TextStyle(
+    fontFamily: kInterFontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bodyText = TextStyle(
+    fontFamily: kInterFontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle bodyTextSmall = TextStyle(
+    fontFamily: kInterFontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle bodyTextVerySmall = TextStyle(
+    fontFamily: kInterFontFamily,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+}
