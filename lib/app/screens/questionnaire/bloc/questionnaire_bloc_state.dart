8d4bd73a941ac134cae7f4f1e4dcbb2f148ed11d
vishelpodishat <alishsaideshov@gmail.com>
2024-08-05 14:55:47 +0500

    bloc changes

--- lib/app/screens/questionnaire/bloc/questionnaire_bloc_state.dart
+++ lib/app/screens/questionnaire/bloc/questionnaire_bloc_state.dart
@@ -1,23 +1,38 @@
-part of 'questionnaire_bloc_bloc.dart';
+part of 'questionnaire_bloc.dart';
 
 @immutable
-sealed class QuestionnaireBlocState {}
+sealed class QuestionnaireState {}
 
-final class QuestionnaireBlocInitial extends QuestionnaireBlocState {}
+final class QuestionnaireBlocInitial extends QuestionnaireState {}
 
-class QuestionnaireLoadingState extends QuestionnaireBlocState {}
+class QuestionnaireLoadingState extends QuestionnaireState {}
 
-class QuestionnaireSuccessState extends QuestionnaireBlocState {
-  final List<QuestionnaireModel> questions;
+class QuestionnaireSuccessState extends QuestionnaireState {
+  final List<Question> questions;
   final int currentIndex;
+  final String? selectedAnswer;
   QuestionnaireSuccessState(
-      {required this.questions, required this.currentIndex});
+      {required this.questions,
+      required this.currentIndex,
+      this.selectedAnswer});
+
+  QuestionnaireSuccessState copyWith({
+    List<Question>? questions,
+    int? currentIndex,
+    String? selectedAnswer,
+  }) {
+    return QuestionnaireSuccessState(
+      questions: questions ?? this.questions,
+      currentIndex: currentIndex ?? this.currentIndex,
+      selectedAnswer: selectedAnswer ?? this.selectedAnswer,
+    );
+  }
 }
 
-class QuestionnaireErrorState extends QuestionnaireBlocState {
+class QuestionnaireErrorState extends QuestionnaireState {
   final String errorMessage;
 
   QuestionnaireErrorState(this.errorMessage);
 }
 
-class QuestionnaireCompletedState extends QuestionnaireBlocState {}
+class QuestionnaireCompletedState extends QuestionnaireState {}
