d96b5534f4e37055fbe79caedf90e21e4016e6ad
vishelpodishat <alishsaideshov@gmail.com>
2024-08-12 23:07:30 +0500

    localization for questionnaire

--- lib/app/screens/questionnaire/model/questionnaire_model.dart
+++ lib/app/screens/questionnaire/model/questionnaire_model.dart
@@ -1,3 +1,6 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
 class QuestionnaireModel {
   String? testId;
   String? testType;
@@ -71,6 +74,17 @@ class Titles {
     data['ru'] = ru;
     data['kk'] = kk;
     return data;
+  }
+
+  String getLocalizedString(BuildContext context) {
+    final locale = context.locale.languageCode;
+    if (locale == 'ru') {
+      return ru ?? kk!;
+    } else if (locale == 'kk') {
+      return kk ?? ru!;
+    } else {
+      return en ?? ru ?? kk ?? '';
+    }
   }
 }
 
