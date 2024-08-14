56f01bc9a8c36979f9058432ddb8578c4984d675
vishelpodishat <alishsaideshov@gmail.com>
2024-08-14 15:38:35 +0500

    for seeing my screen

--- lib/app/screens/profile/profile_screen.dart
+++ lib/app/screens/profile/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wg_app/app/screens/psytest/screens/description_test_screen.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -8,6 +9,13 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Text('Profile Screen /screens/profile/profile_screen.dart'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => DescriptionTestScreen()),
+          );
+        },
       ),
     );
   }
