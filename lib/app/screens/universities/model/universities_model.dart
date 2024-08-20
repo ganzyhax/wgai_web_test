ca74c48bee89e75ce37c6efc3b63d87945060df3
vishelpodishat <alishsaideshov@gmail.com>
2024-08-20 15:59:43 +0500

    model

---
+++ lib/app/screens/universities/model/universities_model.dart
@@ -0,0 +1,13 @@
+class University {
+  final String codeNumber;
+  final String title;
+  final String firstDescription;
+  final String secondDescription;
+
+  University({
+    required this.codeNumber,
+    required this.title,
+    required this.firstDescription,
+    required this.secondDescription,
+  });
+}
