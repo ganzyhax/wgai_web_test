a86a55a163775e57a00afe9a4c148e82091548f6
vishelpodishat <alishsaideshov@gmail.com>
2024-08-28 23:52:26 +0500

    species

--- lib/app/screens/specialities/specialities_screen.dart
+++ lib/app/screens/specialities/specialities_screen.dart
@@ -12,107 +12,107 @@ class SpecialitiesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
         backgroundColor: AppColors.background,
-        appBar: AppBar(
-          backgroundColor: AppColors.background,
 
-          title: Text(
-            'Специальности',
-            style: AppTextStyle.titleHeading
-                .copyWith(color: AppColors.blackForText),
-          ),
-          //        BlocBuilder<SpecialitiesBloc, SpecialitiesState>(
+        title: Text(
+          'Специальности',
+          style:
+              AppTextStyle.titleHeading.copyWith(color: AppColors.blackForText),
+        ),
+        //        BlocBuilder<SpecialitiesBloc, SpecialitiesState>(
+        //   builder: (context, state) {
+        //     if (state is SpecialitiesLoaded) {
+
+        //     } else {
+        //       const CircularProgressIndicator();
+        //     }
+        //   },
+        // )
+      ),
+      body:
+          //BlocBuilder<SpecialitiesBloc, SpecialitiesState>(
           //   builder: (context, state) {
           //     if (state is SpecialitiesLoaded) {
 
-          //     } else {
-          //       const CircularProgressIndicator();
           //     }
           //   },
-          // )
-        ),
-        body:
-            //BlocBuilder<SpecialitiesBloc, SpecialitiesState>(
-            //   builder: (context, state) {
-            //     if (state is SpecialitiesLoaded) {
-
-            //     }
-            //   },
-            // ),
-            Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Специальности',
-                    style: AppTextStyle.heading2
-                        .copyWith(color: AppColors.blackForText),
-                  ),
-                  Container(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
-                    decoration: BoxDecoration(
-                      color: AppColors.filterGray,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Все предметы',
-                          style: AppTextStyle.heading4
-                              .copyWith(color: AppColors.calendarTextColor),
-                        ),
-                        SizedBox(width: 4),
-                        SvgPicture.asset('assets/icons/expand_down.svg')
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: 12),
-              Expanded(
-                child: BlocBuilder<SpecialitiesBloc, SpecialitiesState>(
-                  builder: (context, state) {
-                    if (state is SpecialitiesLoading) {
-                      return Center(child: CircularProgressIndicator());
-                    } else if (state is SpecialitiesLoaded) {
-                      return ListView.builder(
-                        itemCount: state.specialResources?.length,
-                        itemBuilder: (context, index) {
-                          final specialResources =
-                              state.specialResources?[index];
-                          return Padding(
-                            padding: const EdgeInsets.only(bottom: 8),
-                            child: UniContainers(
-                              codeNumber: specialResources?.code ?? '',
-                              title: specialResources?.name
-                                      ?.getLocalizedString(context) ??
-                                  '',
-                              firstDescription: specialResources
-                                      ?.profileSubjects?[0].name
-                                      ?.getLocalizedString(context) ??
-                                  '',
-                              secondDescription: specialResources
-                                      ?.grants?.general?.grantsTotal ??
-                                  0,
-                              onTap: () {},
-                            ),
-                          );
-                        },
-                      );
-                    } else if (state is SpecialitiesError) {
-                      return Center(child: Text(state.message));
-                    } else {
-                      return Container();
-                    }
-                  },
+          // ),
+          Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Специальности',
+                  style: AppTextStyle.heading2
+                      .copyWith(color: AppColors.blackForText),
                 ),
-              )
-            ],
-          ),
-        ));
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: AppColors.filterGray,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Все предметы',
+                        style: AppTextStyle.heading4
+                            .copyWith(color: AppColors.calendarTextColor),
+                      ),
+                      SizedBox(width: 4),
+                      SvgPicture.asset('assets/icons/expand_down.svg')
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 12),
+            Expanded(
+              child: BlocBuilder<SpecialitiesBloc, SpecialitiesState>(
+                builder: (context, state) {
+                  if (state is SpecialitiesLoading) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (state is SpecialitiesLoaded) {
+                    return ListView.builder(
+                      itemCount: state.specialResources?.length,
+                      itemBuilder: (context, index) {
+                        final specialResources = state.specialResources?[index];
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 8),
+                          child: UniContainers(
+                            codeNumber: specialResources?.code ?? '',
+                            title: specialResources?.name
+                                    ?.getLocalizedString(context) ??
+                                '',
+                            firstDescription: specialResources
+                                    ?.profileSubjects?[0].name
+                                    ?.getLocalizedString(context) ??
+                                '',
+                            secondDescription: specialResources
+                                    ?.grants?.general?.grantsTotal ??
+                                0,
+                            onTap: () {},
+                          ),
+                        );
+                      },
+                    );
+                  } else if (state is SpecialitiesError) {
+                    return Center(child: Text(state.message));
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
