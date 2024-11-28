713c2f35e11a124d4d6cf81bf66acc8761db9c37
unknown <muxammederaliev@gmail.com>
2024-11-28 21:49:01 +0600

    added fix notification

--- lib/app/screens/community/community_screen.dart
+++ lib/app/screens/community/community_screen.dart
@@ -36,96 +36,104 @@ class _CommunityScreenState extends State<CommunityScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CommunityBloc, CommunityState>(
-      builder: (context, state) {
+    return BlocListener<CommunityBloc, CommunityState>(
+      listener: (context, state) {
         if (state is CommunityLoaded) {
-          if (!state.isOpened) {
-            if (widget.isCounsulant != null) {
-              if (widget.isCounsulant == false) {
+          _pageController.jumpToPage(state.selectedTabIndex);
+        }
+      },
+      child: BlocBuilder<CommunityBloc, CommunityState>(
+        builder: (context, state) {
+          if (state is CommunityLoaded) {
+            if (!state.isOpened) {
+              if (widget.isCounsulant != null) {
+                if (widget.isCounsulant == false) {
+                  BlocProvider.of<CommunityBloc>(context)
+                    ..add(CommunitySelectTabIndex(selectedTabIndex: 0));
+                }
+                if (widget.isCounsulant == true) {
+                  BlocProvider.of<CommunityBloc>(context)
+                    ..add(CommunitySelectTabIndex(selectedTabIndex: 1));
+                }
                 BlocProvider.of<CommunityBloc>(context)
-                  ..add(CommunitySelectTabIndex(selectedTabIndex: 0));
+                  ..add(CommunitySetIsOpened());
               }
-              if (widget.isCounsulant == true) {
-                BlocProvider.of<CommunityBloc>(context)
-                  ..add(CommunitySelectTabIndex(selectedTabIndex: 1));
-              }
-              BlocProvider.of<CommunityBloc>(context)
-                ..add(CommunitySetIsOpened());
             }
-          }
-          return Scaffold(
-            backgroundColor: AppColors.background,
-            appBar: AppBar(
-              surfaceTintColor: Colors.transparent,
+
+            return Scaffold(
               backgroundColor: AppColors.background,
-              title: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        BlocProvider.of<CommunityBloc>(context)
-                          ..add(CommunitySelectTabIndex(selectedTabIndex: 0));
-                        _pageController.animateToPage(0,
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.ease);
-                      },
-                      child: Text(
-                        LocaleKeys.feed.tr(),
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.w800,
-                          color: (state.selectedTabIndex == 0)
-                              ? Colors.black
-                              : Colors.grey[400],
+              appBar: AppBar(
+                surfaceTintColor: Colors.transparent,
+                backgroundColor: AppColors.background,
+                title: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          BlocProvider.of<CommunityBloc>(context)
+                            ..add(CommunitySelectTabIndex(selectedTabIndex: 0));
+                          _pageController.animateToPage(0,
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.ease);
+                        },
+                        child: Text(
+                          LocaleKeys.feed.tr(),
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w800,
+                            color: (state.selectedTabIndex == 0)
+                                ? Colors.black
+                                : Colors.grey[400],
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      width: 25,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        BlocProvider.of<CommunityBloc>(context)
-                          ..add(CommunitySelectTabIndex(selectedTabIndex: 1));
-                        _pageController.animateToPage(1,
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.ease);
-                      },
-                      child: Text(
-                        LocaleKeys.consultant.tr(),
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.w800,
-                          color: (state.selectedTabIndex == 1)
-                              ? Colors.black
-                              : Colors.grey[400],
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          BlocProvider.of<CommunityBloc>(context)
+                            ..add(CommunitySelectTabIndex(selectedTabIndex: 1));
+                          _pageController.animateToPage(1,
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.ease);
+                        },
+                        child: Text(
+                          LocaleKeys.consultant.tr(),
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w800,
+                            color: (state.selectedTabIndex == 1)
+                                ? Colors.black
+                                : Colors.grey[400],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            body: PageView(
-              controller: _pageController,
-              onPageChanged: (index) {
-                BlocProvider.of<CommunityBloc>(context)
-                  ..add(CommunitySelectTabIndex(selectedTabIndex: index));
-              },
-              children: [
-                (widget.scrollId != null)
-                    ? NewsScreen(newsID: widget.scrollId)
-                    : NewsScreen(),
-                const ConsultantPage(),
-              ],
-            ),
+              body: PageView(
+                controller: _pageController,
+                onPageChanged: (index) {
+                  BlocProvider.of<CommunityBloc>(context)
+                    ..add(CommunitySelectTabIndex(selectedTabIndex: index));
+                },
+                children: [
+                  (widget.scrollId != null)
+                      ? NewsScreen(newsID: widget.scrollId)
+                      : NewsScreen(),
+                  const ConsultantPage(),
+                ],
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
           );
-        }
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
+        },
+      ),
     );
   }
 }
